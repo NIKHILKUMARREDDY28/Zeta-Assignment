8b2055a3e496b24e8b676d1d15cd83f35a095916
NIKHIL KUMAR REDDY <nikhilkumar.m@skil.ai>
2025-05-01 07:31:56 +0530

    Refactor

--- app/task2/main.py
+++ app/task2/main.py
@@ -73,4 +73,4 @@ def create_dispute(new: NewDispute):
     return disc
 
 if __name__ == "__main__":
-    uvicorn.run("task2_backend:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000)
